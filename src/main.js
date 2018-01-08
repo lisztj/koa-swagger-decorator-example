db7f1e0284eed28c504637d7b588cf25ac7f9790
赵健 <mr.91@foxmail.com>
2018-01-08 14:33:47 +0800

    comment

--- src/main.js
+++ src/main.js
@@ -5,17 +5,18 @@ import cors from 'koa-cors';
 import config from 'config';
 import errorHandle from 'middleware/errorHandle';
 import router from 'routes/index';
+// import convert from 'koa-convert';
 
 const serve = require('koa-static');
-
+const convert = require('koa-convert');
 const app = new Koa();
 
-app
-.use(cors())
-.use(serve('.'))
-.use(bodyParser())
-.use(errorHandle())
-.use(router.routes())
-.use(router.allowedMethods());
+app.use(cors())
+  .use(serve('.'))
+  .use(convert())
+  .use(bodyParser())
+  .use(errorHandle())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(config.port);
